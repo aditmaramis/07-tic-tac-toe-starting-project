b07a8ec9e7cb8ee538d96f275455237ba1bb6bc5
aditmaramis <adit.maramis@gmail.com>
2026-02-22 12:44:47 +0700

    feat: implement Player component for managing player names and editing functionality

---
+++ src/components/Player.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+
+export default function Player({ initialName, symbol }) {
+	const [isEditing, setIsEditing] = useState(false);
+	const [playerName, setPlayerName] = useState(initialName);
+
+	function handleEdit() {
+		setIsEditing((isEditing) => !isEditing);
+	}
+
+	function handleNameChange(event) {
+		setPlayerName(event.target.value);
+	}
+
+	let editablePlayerName = <span className="player-name">{playerName}</span>;
+	// let btnCaption = 'Edit';
+
+	if (isEditing) {
+		editablePlayerName = (
+			<input
+				type="text"
+				required
+				value={playerName}
+				onChange={handleNameChange}
+			/>
+		);
+		// btnCaption='Save';
+	}
+
+	return (
+		<li>
+			<span className="player">
+				{editablePlayerName}
+				<span className="player-symbol">{symbol}</span>
+			</span>
+			<button onClick={handleEdit}>{isEditing ? 'Save' : 'Edit'}</button>
+		</li>
+	);
+}
